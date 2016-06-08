8f41f974fdeb2b6acdaf94717d130a2801882603
Arlen Cho <arlen_cho@hotmail.com>
2016-06-08 10:32:27 -0400

    Animation Classwork

---
+++ magic-eight/starter.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+  $('button').on("click", function() {
+    console.log('hello')
+    $('img').removeClass();
+    setTimeout(function(){$('img').addClass('animated zoomIn')}, 1
+    );
+  });
+});
